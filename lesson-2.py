c08af1e4d2c6a61ee59650f8c96402ec385f02ce
kocetjs <rony.cetzl@gmail.com>
2020-04-28 16:00:36 +0700

    Meretas sandi Caesar dengan Brute-force

---
+++ lesson-2.py
@@ -0,0 +1,44 @@
+###############################################################
+# Sandi Caesar
+# Editor : Matius Celcius Sinaga
+# Author : # http://inventwithpython.com/hacking (BSD Licensed)
+###############################################################
+
+import pyperclip
+
+pesan = 'Indonesia Raya'
+kunci = 9
+mode = 'encrypt'
+HURUF ='ABCDEFGHIJKLMNOPWRSTUVWXYZ'
+ubah = ''
+
+pesan = pesan.upper()
+
+for symbol in pesan:
+    if symbol in HURUF:
+
+        nomor = HURUF.find(symbol)
+
+        if mode == 'encrypt':
+            nomor = nomor + kunci
+        elif mode == 'decrypt':
+            nomor = nomor - kunci
+
+        if nomor >= len(HURUF):
+            nomor = nomor - len(HURUF)
+        elif nomor < 0:
+            nomor = nomor + len(HURUF)
+
+        ubah = ubah + HURUF[nomor]
+
+    else:
+
+        ubah = ubah + symbol
+
+print(ubah)
+
+print(range(len(HURUF)))
+
+#menyalin hasil enkripsi/dekripsi pada penyimpanan sementara
+pyperclip.copy(ubah)
+
---
+++ lesson-3.py
@@ -0,0 +1,30 @@
+################################################################
+# Meretas sandi Caesar dengan Brute-force
+# Editor : Matius Celcius Sinaga
+# Author : # http://inventwithpython.com/hacking (BSD Licensed)
+#################################################################
+
+pesan = 'RWMXWNBRJ AJHJ'
+HURUF = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for kunci in range(len(HURUF)):
+
+    ubah = ''
+
+    for symbol in pesan:
+        if symbol in HURUF:
+
+            nomor = HURUF.find(symbol)
+            nomor = nomor - kunci
+
+            if nomor < 0:
+
+                nomor = nomor + len(HURUF)
+
+            ubah = ubah + HURUF[nomor]
+
+        else:
+
+            ubah = ubah + symbol
+
+    print('Key #%s: %s' % (kunci, ubah))
