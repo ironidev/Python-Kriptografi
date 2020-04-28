60b1b00aa70ac2ac9b8959b99bf0011f6859c588
kocetjs <rony.cetzl@gmail.com>
2020-04-28 16:30:54 +0700

    MELAKUKAN ENKRIPSI DENGAN SANDI TRANSPOSISI

---
+++ lesson-4.py
@@ -0,0 +1,34 @@
+################################################################
+# Enkripsi Transposisi
+# Editor : Matius Celcius Sinaga
+# Author : # http://inventwithpython.com/hacking (BSD Licensed)
+################################################################
+
+import pyperclip
+
+def main():
+    pesanSaya = 'Indonesia Raya Tanah Airku:IRTA'
+    kunciSaya = 8
+
+    sanditeks = enkripsiPesan(kunciSaya, pesanSaya)
+
+    print(sanditeks + '|')
+
+    pyperclip.copy(sanditeks)
+
+def enkripsiPesan(kunci, pesan):
+    sanditeks = [''] * kunci
+
+    for col in range(kunci):
+
+        pointer = col
+
+        while pointer < len(pesan):
+
+            sanditeks[col] += pesan[pointer]
+            pointer += kunci
+
+    return ''.join(sanditeks)
+
+if __name__ == '__main__':
+    main()
